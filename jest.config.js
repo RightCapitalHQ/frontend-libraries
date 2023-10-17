516f0536b1c632f3a382b48b1a3b4a394c103de7
Bryan Lee <38807139+liby@users.noreply.github.com>
2023-10-17 14:08:11 +0800

    chore: update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,8 @@
+/**
+ * @type {import('jest').Config}
+ */
 module.exports = {
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
   coverageDirectory: './coverage/',
   collectCoverage: true,
-  preset: 'ts-jest',
   testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
 };
--- packages/date-helpers/jest.config.js
+++
@@ -1,7 +0,0 @@
-process.env.TZ = 'Asia/Beijing';
-
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts$',
-};
